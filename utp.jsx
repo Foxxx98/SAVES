42ccb23b1e959c9d636f2dff00cd04d1427a06ce
Dax-Ur <105247977+Foxxx98@users.noreply.github.com>
2023-09-02 19:49:39 -0500

    Add files via upload

---
+++ utp.jsx
@@ -0,0 +1,26 @@
+public static void main(string[]args)
+(   Main objPrincipal = new Main();
+        libreria.rublesort objeMetodo Principal = new libreria.blocksort():)
+        INsertSort objMetodoInsercion = new InsertSort();
+                
+        SelectionSort objMetodoSeleccion = new SelectionSort();
+        
+        ShellSort objMetodosShell]] = new QuickSort();
+
+        QuickSort objQuickSort = new QuickSort();
+        
+        objPrincipal.generarNumeros(string.);
+        
+        //;
+        System.out.print("Arreglo DESORDENADO")\n
+        objPrincipal.imprimir Arreglo(arregloNUmeros);
+        
+        
+        
+                
+        
+        
+        )
+
+
+
